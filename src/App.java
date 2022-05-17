b156b8c454305332dcd57215e68a8ed95d3fa80d
leventakdag <leventakdag@Levents-MacBook-Pro.local>
2022-05-17 17:34:40 +0300

    my changes

--- src/App.java
+++ src/App.java
@@ -20,6 +20,7 @@ public class App {
             Point p1 = new Point (i, points_coordinates[i][0], points_coordinates[i][1], 2*Math.random()+1);
             points[i] = p1;
         }
+        System.out.println(facs[1].getId());
 
         double[][] distanceMatrix = operator.distanceMatrix(facs, points);
         ArrayList<Facility> openedFacilities = new ArrayList<>();
