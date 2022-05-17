e70965bf0e22764ab56f9b529a75ae2f5dd631f6
burakkara999 <63243552+burakkara999@users.noreply.github.com>
2022-05-17 17:14:12 +0300

    Comment line added

--- src/App.java
+++ src/App.java
@@ -10,14 +10,17 @@ public class App {
         double[][] points_coordinates = operator.createDistanceMatrix(20,2);
         Facility[] facs = new Facility[5];
         Point[] points = new Point [20];
+
         for (int i=0; i<facs.length;i++){
             Facility f1 = new Facility(i, facs_coordinates[i][0], facs_coordinates[i][1], 20*Math.random()+50);
             facs[i] = f1;
         }
+
         for (int i=0; i<points.length;i++){
             Point p1 = new Point (i, points_coordinates[i][0], points_coordinates[i][1], 2*Math.random()+1);
             points[i] = p1;
         }
+
         double[][] distanceMatrix = operator.distanceMatrix(facs, points);
         ArrayList<Facility> openedFacilities = new ArrayList<>();
         ArrayList<Point> unassignedPoints = new ArrayList<>();
